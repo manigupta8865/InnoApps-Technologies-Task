a18c10ccdb2765439ea646a6c7bbc371b8eb52dd
Munish Kumar <manigupta8865@gmail.com>
2020-07-20 22:41:32 +0530

    build folder allowed to push in git to deploye in production

---
+++ build/precache-manifest.750983295c314f481589c657af24263e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b982557ec022d2ed5a7348d017d988da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6abd0db97e142d2064e",
+    "url": "/static/css/2.e6f51e71.chunk.css"
+  },
+  {
+    "revision": "decc248f25b59a68668a",
+    "url": "/static/css/main.d067fda9.chunk.css"
+  },
+  {
+    "revision": "e6abd0db97e142d2064e",
+    "url": "/static/js/2.1ffb6fe8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1ffb6fe8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "decc248f25b59a68668a",
+    "url": "/static/js/main.4babde49.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
